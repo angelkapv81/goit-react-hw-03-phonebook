33f1a6d757115b9ac7ba42b573a8b6af72449f39
angelkapv81@gmail.com <angelkapv81>
2023-05-15 16:07:26 +0300

    add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,88 +1,131 @@
-import React from 'react';
-import { nanoid } from 'nanoid';
-import { GlobalStyle } from './GlobalStyle';
-import Form from './Form/Form';
-import Search from './Search/Search';
-import Contacts from './Contacts/Contacts';
-import swal from 'sweetalert';
+import { Component } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { ToastContainer, toast } from 'react-toastify';
+import { Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-class App extends React.Component {
+import SearhBar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import LoadMoreButton from './Button/Button';
+import { AppContainer } from './App.styled';
+import fetchApi from '../components/ApiService/ApiService';
+import Spiner from './Loader/Loader';
+import Modal from './Modal/Modal';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+export default class App extends Component {
+  static propTypes = { searchQuery: PropTypes.string };
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    name: '',
-    filter: '',
+    searchQuery: '',
+    images: [],
+    page: 1,
+    selectedImage: null,
+    alt: null,
+    status: 'idle',
+    error: null,
   };
-  // перше завантаження
-  componentDidMount() {
-    const localData = localStorage.getItem('contacts');
-    if (localData) {
-      this.setState({ contacts: JSON.parse(localData) });
-    } else {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  // КОЛИ ОНОВЛЮЄТЬСЯ ДОМ
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  totalHits = null;
 
-  addContact = (name, number) => {
-    this.setState({ filter: '' });
-    if (
-      this.state.contacts.some(
-        el => el.name.toLowerCase() === name.toLowerCase()
-      )
-    ) {
-      swal(`Contact ${name} already in phonebook!`);
+  async componentDidUpdate(_, prevState) {
+    const { page, searchQuery } = this.state;
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ status: 'pending' });
+
+      try {
+        const imageData = await fetchApi(searchQuery, page);
+        this.totalHits = imageData.total;
+        const imagesHits = imageData.hits;
+        if (!imagesHits.length) {
+          toast.warning(
+            'No results were found for your search, please try something else.',
+            { transition: Zoom, position: 'top-center' }
+          );
+        }
+        this.setState(({ images }) => ({
+          images: [...images, ...imagesHits],
+          status: 'resolved',
+        }));
+
+        if (page > 1) {
+          const CARD_HEIGHT = 300; // preview image height
+          window.scrollBy({
+            top: CARD_HEIGHT * 2,
+            behavior: 'smooth',
+          });
+        }
+      } catch (error) {
+        toast.error(`Sorry something went wrong. ${error.message}`);
+        this.setState({ status: 'rejected' });
+      }
+    }
+  }
+  handleFormSubmit = searchQuery => {
+    if (this.state.searchQuery === searchQuery) {
       return;
     }
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    this.setState(prevState => {
-      return {
-        contacts: [contact, ...prevState.contacts],
-      };
+    this.resetState();
+    this.setState({ searchQuery });
+  };
+  handleSelectedImage = (largeImageUrl, tags) => {
+    this.setState({
+      selectedImage: largeImageUrl,
+      alt: tags,
     });
   };
 
-  deleteContact = contactId => {
+  resetState = () => {
+    this.setState({
+      searchQuery: '',
+      page: 1,
+      images: [],
+      selectedImage: null,
+      alt: null,
+      status: 'idle',
+    });
+  };
+
+  loadMore = () => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+      page: prevState.page + 1,
     }));
   };
 
-  handleFilterChange = event => {
-    const filterValue = event.target.value;
-    this.setState({ filter: filterValue });
+  closeModal = () => {
+    this.setState({
+      selectedImage: null,
+    });
   };
 
   render() {
-    const filteredContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-    );
-
+    const { images, status, selectedImage, alt, error } = this.state;
     return (
-      <section>
-        <Form onSubmit={this.addContact} />
-        <Search
-          filterQuery={this.state.filter}
-          filter={this.handleFilterChange}
-        />
-        <Contacts contacts={filteredContacts} onDelete={this.deleteContact} />
-        <GlobalStyle />
-      </section>
+      <AppContainer>
+        <SearhBar onSubmit={this.handleFormSubmit} />
+        <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
+        {status === 'pending' && <Spiner />}
+        {error && (
+          <h1 style={{ color: 'orangered', textAlign: 'center' }}>
+            {error.message}
+          </h1>
+        )}
+        {images.length > 0 && (
+          <ImageGallery
+            images={images}
+            selectedImage={this.handleSelectedImage}
+          />
+        )}
+        {images.length > 0 && images.length !== this.totalHits && (
+          <LoadMoreButton onClick={this.loadMore} />
+        )}
+        {selectedImage && (
+          <Modal
+            selectedImage={selectedImage}
+            tags={alt}
+            onClose={this.closeModal}
+          />
+        )}
+      </AppContainer>
     );
   }
 }
-export default App;
